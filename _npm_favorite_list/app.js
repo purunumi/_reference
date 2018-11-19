cd6122ca4f6087e6da5e8321f52fa63d3549438e
hanasari <hanasari@stunitas.com>
2018-11-19 18:56:49 +0900

    update

---
+++ _npm_favorite_list/app.js
@@ -0,0 +1,4 @@
+// import {MDCComponent, MDCFoundation} from '@material/base';
+import {MDCRipple} from '@material/ripple/index';
+
+// const ripple = new MDCRipple(document.querySelector('.foo-button'));
