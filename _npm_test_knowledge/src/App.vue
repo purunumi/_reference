fc0a28a6c6c1becc4a3a00b0bfc83ef05143375c
hanasari <hanasari@stunitas.com>
2018-12-14 12:20:19 +0900

    update

--- _npm_test_knowledge/src/App.vue
+++ _npm_test_knowledge/src/App.vue
@@ -12,20 +12,19 @@
 
 
 
-<div class="mdc-form-field">
-
 
 <div class="mdc-text-field">
-  <input type="text" id="my-text-field" class="mdc-text-field__input">
-  <label class="mdc-floating-label" for="my-text-field">Hint text</label>
+  <input type="text" id="inpt-id" class="mdc-text-field__input">
+  <label class="mdc-floating-label" for="inpt-id">ID</label>
   <div class="mdc-line-ripple"></div>
 </div>
 
 
-</div>
-
-
-
+<!-- <div class="mdc-text-field">
+  <input type="password" id="inpt-pwd" class="mdc-text-field__input">
+  <label class="mdc-floating-label" for="inpt-pwd">Password</label>
+  <div class="mdc-line-ripple"></div>
+</div> -->
 
 
 
@@ -39,9 +38,8 @@
 </template>
 
 <script>
-import {MDCFormField} from '@material/form-field';
 import {MDCTextField} from '@material/textfield';
-import {MDCTextFieldHelperText} from '@material/textfield/helper-text';
+import {MDCFloatingLabel} from '@material/floating-label';
 
 export default {
   name: 'app',
@@ -51,39 +49,20 @@ export default {
     }
   },
   beforeDestroy: function(){
-    const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+    // const floatingLabels =  [].map.call(document.querySelectorAll('.mdc-floating-label'), function(el) {
+    //                           return new MDCFloatingLabel(el);
+    //                         });
+    // const textFields =  [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
+    //                       return new MDCTextField(el);
+    //                     });
+
+    const floatingLabel = new MDCFloatingLabel(document.querySelector('.mdc-floating-label'));
     const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-    const helperText = new MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
   }
 }
 
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
